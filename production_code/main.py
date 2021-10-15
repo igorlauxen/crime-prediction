77fbd913985cbb51fe46339eed3f0bf5079ca74f
Igor Lauxen <igor.lauxen@sap.com>
2021-10-15 11:19:20 -0300

    continue data preparation

--- production_code/main.py
+++ production_code/main.py
@@ -1,10 +1,12 @@
-# Pandas is used for data manipulation
 import pandas as pd
-from DateFormatter import DateFormatter
-from TextFormatter import TextFormatter
+import seaborn as sns
 
-dateFormatter = DateFormatter()
-textFormatter = TextFormatter()
+from DataPreparator import DataPreparator
+from MapAnalyser import MapAnalyser
+
+dataPreparator = DataPreparator()
+mapAnalyser = MapAnalyser()
+
 
 # Read in data and get dataframe
 # https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html
@@ -12,38 +14,19 @@ textFormatter = TextFormatter()
 oc_df = pd.read_csv('./data/generated_file_aa.csv',index_col ="ID")
 print(oc_df.head(5))
 
-# we may review this
-# depending on the granularity of these properties
-# we may leave one with the assumption of the proximity of crimes
-# Removing Location Description because its description varies A LOT
-dropped_columns = ["IUCR", "Beat", "District", "Ward", "Community Area", "FBI Code", "X Coordinate", "Y Coordinate", "Updated On", "Case Number", "Location", "Location Description"]
 
-# remove the following columns
-# based on https://www.geeksforgeeks.org/python-delete-rows-columns-from-dataframe-using-pandas-drop/
-oc_df.drop(dropped_columns, axis = 1, inplace = True)
-# check how the data looks like after changes
-print("Depois das mudanças")
-print(oc_df.head(5))
-print("Quantas entradas existem?")
-print(len(oc_df.index))
+sns.scatterplot(oc_df['Latitude'], oc_df['Longitude'])
 
-# drop rows that are inconsistent
-# https://stackoverflow.com/questions/44548721/remove-row-with-null-value-from-pandas-data-frame/44548976
-print("\n\nRemover dados inconsistentes: qualquer coisa com null")
-clean_data_frame = oc_df.dropna(how='any',axis=0) 
-print("Quantas entradas existem apos limpeza dos dados?")
-print(len(clean_data_frame.index))
+# Data Preparation
+labels, feature_list, features_np_array, train_features, test_features, train_labels, test_labels = dataPreparator.prepare(oc_df)
+# Baseline
+mapAnalyser.createMap(oc_df)
 
-# random forest seems to expect all columns to be numbers
-# as described in here: https://towardsdatascience.com/random-forest-in-python-24d0893d51c0
-# due do this we need to convert the string columns to be something meaninful to the RF
-# Non number columns: Primary Type, Description, Location Description, Arrest, Domestic, Date
 
-clean_data_frame = dateFormatter.format(clean_data_frame)
+# RF execution
 
-print("Como ficou o dataframe após as datas?")
-print(clean_data_frame.columns)
-
-clean_data_frame = textFormatter.format(clean_data_frame)
-print("Como ficou o dataframe após os textos?")
-print(clean_data_frame.columns)
+# In the end game I want to predict what crime can happen and where it will 
+# But first I'll split them into two different predictions: 
+#   1) tell where it will happen 
+#   2) tell what will happen
+#   3) Tell me what will happen and where
--- readme.md
+++ readme.md
@@ -6,19 +6,32 @@ O objetivo é realizar uma predição de crimes em data área baseado no histór
 
 O algoritmo a ser considerado é [Random Forest](https://en.wikipedia.org/wiki/Random_forest)
 
+Inspirado pesadamente nos tutoriais:
+
+1. [Toward Data Science: random forest in python](https://towardsdatascience.com/random-forest-in-python-24d0893d51c0)
+1. [Working with Geospatial Data in ML](https://heartbeat.comet.ml/working-with-geospatial-data-in-machine-learning-ad4097c7228d)
+
 ## Pre-Requisitos para executar
 
 - [VSCode]()
 - [Pycharm]()
 - [Python3]()
+- [Pip3](https://stackoverflow.com/questions/6587507/how-to-install-pip-with-python-3)
+  - install the following libs:
+    - `pip3 install folium`
+    - `pip3 install geopy`
 
 ## TODO list
 
-- [ ] Decidir quais colunas do Excel vão ser consideradas
+- [X] Decidir quais colunas do Excel vão ser consideradas
 - [ ] Executar RF com o mínimo possível de dados
-  - Colunas mínimas: Primary Type, Latitude, Longitude, todas as colunas de data (após formato)
+  - [X] Colunas mínimas: Primary Type, Latitude, Longitude, todas as colunas de data (após formato)
+- [X] Tem que converter campos booleans para numéricos
 - [ ] Adicionar ao RF todas as colunas String necessárias
-  - Colunas necessárias: Primary Type, Description
+  - Colunas necessárias: Primary Type, Description, Location
+- [ ] Criar/Valuda uma coluna coluna para medir a distancia dos pontos passa saber se já aconteceu um crime perto
+- [ ] Necessário revisar as datas para transformar em seno e cosseno talvez?
+  - Baseado em [Feature Engineering Cyclical Features](http://blog.davidkaleko.com/feature-engineering-cyclical-features.html)
 
 ## Nice to know
 
